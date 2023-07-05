ea43ee8dc2a8d08d5a623bf021111a3fce4b40b4
Vincent Darwinkel <vincent.darwinkel@hotmail.nl>
2023-07-05 17:58:19 +0200

    Added more code

--- Laser.cpp
+++ Laser.cpp
@@ -1,7 +1,161 @@
 #include "Laser.h"
-#include <NativeEthernet.h>
+#include "math.h"
+#include "Dac.h"
+#include "Basics.h"
 
-Laser::Laser() {}
+MCP4X dac;
+
+Laser::Laser() {
+  _quality = FROM_FLOAT(1. / (LASER_QUALITY));
+
+  _x = 0;
+  _y = 0;
+  _oldX = 0;
+  _oldY = 0;
+
+  _state = 0;
+
+  _scale = 1;
+  _offsetX = 0;
+  _offsetY = 0;
+
+  _moved = 0;
+  _maxMove = -1;
+  _laserForceOff = false;
+  resetClipArea();
+
+  _enable3D = false;
+  _zDist = 1000;
+}
+
+void Laser::init(float scale, long offsetX, long offsetY)
+{
+  _scale = FROM_FLOAT(scale);
+  _offsetX = offsetX;
+  _offsetY = offsetY;
+
+  dac.init(MCP4X_4822, 5000, 5000, 10, 7, 1);
+  dac.setGain2x(MCP4X_CHAN_A, 0);
+  dac.setGain2x(MCP4X_CHAN_B, 0);
+  dac.begin(1);
+}
+
+void Laser::sendToDAC(int x, int y)
+{
+#ifdef LASER_SWAP_XY
+  int x1 = y;
+  int y1 = x;
+#else
+  int x1 = x;
+  int y1 = y;
+#endif
+#ifdef LASER_FLIP_X
+  x1 = 4095 - x1;
+#endif
+#ifdef LASER_FLIP_Y
+  y1 = 4095 - y1;
+#endif
+  dac.output2(x1, y1);
+}
+
+void Laser::resetClipArea()
+{
+  _clipXMin = 0;
+  _clipYMin = 0;
+  _clipXMax = 4095;
+  _clipYMax = 4095;
+}
+
+void Laser::setClipArea(int x, int y, int x1, int y1)
+{
+  _clipXMin = x;
+  _clipYMin = y;
+  _clipXMax = x1;
+  _clipYMax = y1;
+}
+
+/**
+   @brief sets the input value to the max or min value if it goes under or over the min and max values.
+
+   @param input the input variable to fix
+   @param min the minimum allowed value
+   @param max the maximum allowed value
+   @return int the value between or equal to the min or max value
+*/
+int Laser::fixBoundary(int input, int min, int max)
+{
+  if (input < min)
+  {
+    input = min;
+  }
+  if (input > max)
+  {
+    max = max;
+  }
+  return input;
+}
+
+/**
+   @brief sends the mirrors of the galvo to the specified location
+
+   @param xpos the position of the x mirror in the galvo
+   @param ypos the position of the y mirror in the galvo
+*/
+void Laser::sendTo(int xPos, int yPos)
+{
+  xPos = fixBoundary(xPos, -4000, 4000);
+  yPos = fixBoundary(yPos, -4000, 4000);
+
+  int delayMicrosecondsTimeX = (_xPos > xPos ? _xPos - xPos : xPos - _xPos) / 3;
+  int delayMicrosecondsTimeY = (_yPos > yPos ? _yPos - yPos : yPos - _yPos) / 3;
+  int delayTime = delayMicrosecondsTimeX > delayMicrosecondsTimeY ? delayMicrosecondsTimeX : delayMicrosecondsTimeY;
+  if (delayTime < 25) {
+    delayTime = 25;
+  }
+  if (delayTime > 1000) {
+    delayTime = 1000;
+  }
+
+  _xPos = xPos;
+  _yPos = yPos;
+
+  long xNew = TO_INT(_xPos * _scale) + _offsetX;
+  long yNew = TO_INT(_yPos * _scale) + _offsetY;
+
+  sendtoRaw(xNew, yNew);
+  delayMicroseconds(delayTime);
+}
+
+void Laser::sendtoRaw(int xNew, int yNew)
+{
+  // devide into equal parts, using _quality
+  long fdiffx = xNew - _x;
+  long fdiffy = yNew - _y;
+  long diffx = TO_INT(abs(fdiffx) * _quality);
+  long diffy = TO_INT(abs(fdiffy) * _quality);
+
+  // use the bigger direction
+  if (diffx < diffy)
+  {
+    diffx = diffy;
+  }
+
+  fdiffx = FROM_INT(fdiffx) / diffx;
+  fdiffy = FROM_INT(fdiffy) / diffx;
+  // interpolate in FIXPT
+  FIXPT tmpx = 0;
+  FIXPT tmpy = 0;
+  for (int i = 0; i < diffx - 1; i++)
+  {
+    tmpx += fdiffx;
+    tmpy += fdiffy;
+    sendToDAC(_x + TO_INT(tmpx), _y + TO_INT(tmpy));
+  }
+
+  _x = xNew;
+  _y = yNew;
+  sendToDAC(_x, _y);
+}
 
 /**
  * Performs a hardware check for the galvo's and the network connection
--- Laser.h
+++ Laser.h
@@ -2,14 +2,80 @@
 #define LASER_H
 
 #include "Arduino.h"
-#include <NativeEthernet.h>
+#include "Basics.h"
+
+// -- The following flags can be used to fine tune the laser timing
+
+// defines the granularity of the line interpolation. 64 means that each line is split into steps of 64 pixels in the longer direction.
+// setting smaller values will slow down the rendering but will cause more linearity in the galvo movement,
+// setting bigger values will cause faster rendering, but lines will not be straight anymore.
+#define LASER_QUALITY 64
+
+// Defines how long the galvos wait for the on/off toggling of the laser pointer (in microseconds), this will depend on your laser pointer.
+//#define LASER_TOGGLE_DELAY 500
+// Defines how long the galvos wait at the end of a line (currently only used for the 3D cube rendering, in microseconds).
+//#define LASER_LINE_END_DELAY 200
+// Defines the delay the laser waits after reaching a given position (in microseconds).
+//#define LASER_END_DELAY 5
+// Defines the delay after each laser movement (used when interpolating lines, in microseconds), if not defines, 0 is used
+//#define LASER_MOVE_DELAY 5
+
+// -- The following flags can be used to rotate/flip the output without changing the DAC wiring, just uncomment the desired swap/flip
+// define this to swap X and Y on the DAC
+// define this to flip along the x axis
+//#define LASER_FLIP_X
+// define this to flip along the y axis
+#define LASER_SWAP_XY
+//#define LASER_FLIP_X
+#define LASER_FLIP_Y
 
 class Laser {
-  public:
-    Laser();
-    bool hardwareSelfCheck();
+public:
+  Laser();
+  void init(float scale, long offsetX, long offsetY);
+  void sendTo(int x, int y);
+  bool hardwareSelfCheck();
 
-  private:
+private:
+  void resetClipArea();
+  void setClipArea(int x, int y, int x1, int y1);
+  void sendToDAC(int x, int y);
+  void sendtoRaw(int x, int y);
+  int fixBoundary(int input, int min, int max);
+
+  const byte _redLaserPin = 2;
+  const byte _greenLaserPin = 3;
+  const byte _blueLaserPin = 4;
+  int _yPos = 0;
+  int _xPos = 0;
+
+  FIXPT _quality;
+
+  long _x;
+  long _y;
+  int _state;
+
+  FIXPT _scale;
+  long _offsetX;
+  long _offsetY;
+
+  long _moved;
+  long _maxMove;
+  bool _laserForceOff;
+  long _maxMoveX;
+  long _maxMoveY;
+
+  long _oldX;
+  long _oldY;
+
+  long _clipXMin;
+  long _clipYMin;
+  long _clipXMax;
+  long _clipYMax;
+
+  bool _enable3D;
+  Matrix3 _matrix;
+  long _zDist;
 };
 
 #endif
